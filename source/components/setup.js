1bcb595e66981e64cab3618ae5b9aa07d6cbe072
Pascal H <B@example.com>
2023-05-24 14:56:36 -0700

    feat(settings-menu): updated pages with feature

--- source/components/setup.js
+++ source/components/setup.js
@@ -1,7 +1,8 @@
 import AppHeader from './AppHeader.js';
 import HomeSection from './HomeSection.js';
 import SortOrChooseSection from './SortOrChooseSection.js';
-
+import SettingsMenu from './SettingsMenu.js';
 customElements.define('app-header', AppHeader);
 customElements.define('home-section', HomeSection);
 customElements.define('sort-or-choose-section', SortOrChooseSection);
+customElements.define('settings-menu', SettingsMenu);
