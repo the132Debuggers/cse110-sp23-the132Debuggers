db4bea4573d29ebbdc8929f6fd2c411b7a99e3c8
Grace <45552424+yyygrace@users.noreply.github.com>
2023-05-28 14:58:35 -0700

    feat(initial-pages): designed layout for sorting hat page (#81)
    * added sorting hat page
    
    * added back button
    
    * added back button svg
    
    * fix nits
    
    * prettier
    
    ---------
    
    Co-authored-by: Kiyoshi <44930252+Kiyo5hi@users.noreply.github.com>

---
+++ source/components/SortingHatSection.js
@@ -0,0 +1,76 @@
+import { normalize } from '../js/utils.js';
+
+class SortingHatSection extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+
+    const style = new CSSStyleSheet();
+    style.replaceSync(`
+      :host {
+        height: 100%;
+        display: grid;
+        grid-template-columns: repeat(1, minmax(0, 1fr));
+        gap: 0.25rem;
+      }
+
+      .wrapper {
+        display: flex;
+        flex-direction: column;
+        place-items: center;
+        place-content: center;
+      }
+
+      #button{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+      }
+
+      #button h1 {
+        display: inline-block;
+        padding: 0.5rem 1.5rem;
+        background-color: rgba(0, 0, 0, 0.5);
+        border-radius: 15px;
+        border: 1px solid black;
+        color: white;
+        font-size: 2.5rem;
+      }
+
+      #button img {
+        vertical-align: middle;
+        margin-bottom: 2rem;
+        height: 25rem;
+      }
+
+      @media (max-width: 640px) {
+        .button img {
+          height: 12rem;
+          justify-items: center;
+        }
+
+        .button h1 {
+          font-size: 2rem;
+          border-radius: 10px;
+        }
+      }
+    `);
+
+    this.shadowRoot.adoptedStyleSheets = [normalize, style];
+
+    this.shadowRoot.innerHTML = ` 
+        <div class="wrapper">
+            <button id="button"> 
+              <img src="./images/sorting-hat.svg" alt="Image of the sorting hat"/>
+              <h1>CLICK ME TO BE SORTED</h1>
+            </button>
+          <div>
+    `;
+
+    document.querySelector('#app').style.backgroundImage =
+      'url(./images/sorting-hat-bg.png)';
+  }
+}
+
+export default SortingHatSection;
