eb141a514697593254ee743cf89e3cd5603a0abf
Kiyoshi <44930252+Kiyo5hi@users.noreply.github.com>
2023-05-28 15:57:33 -0700

    feat(back-button): designed back button component (#87)
    feat(back-buttom): designed back button component

---
+++ source/components/BackButton.js
@@ -0,0 +1,42 @@
+import { normalize } from '../js/utils.js';
+
+class BackButton extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+
+    const style = new CSSStyleSheet();
+    style.replaceSync(`
+      :host {
+        position: relative;
+        z-index: 10;
+        width: 0;
+        height: 0;
+      }
+
+      #wrapper {
+        position: absolute;
+        bottom: 0.5rem;
+        left: 1rem;
+      }
+
+      svg {
+        width: 2.5rem;
+        opacity: 0.8;
+        mix-blend-mode: lighten;
+      }
+    `);
+
+    this.shadowRoot.innerHTML = `
+      <div id="wrapper">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3" />
+        </svg>
+      </div>
+    `;
+
+    this.shadowRoot.adoptedStyleSheets = [normalize, style];
+  }
+}
+
+export default BackButton;
--- source/components/setup.js
+++ source/components/setup.js
@@ -3,9 +3,11 @@ import HomeSection from './HomeSection.js';
 import SortOrChooseSection from './SortOrChooseSection.js';
 import SortingHatSection from './SortingHatSection.js';
 import HouseSearchSection from './HouseSearchSection.js';
+import BackButton from './BackButton.js';
 
 customElements.define('app-header', AppHeader);
 customElements.define('home-section', HomeSection);
 customElements.define('sort-or-choose-section', SortOrChooseSection);
 customElements.define('sorting-hat-section', SortingHatSection);
 customElements.define('house-search-section', HouseSearchSection);
+customElements.define('back-button', BackButton);
