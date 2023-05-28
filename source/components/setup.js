56ad8bcc3972326c3203b507fa5bec6c181e379c
Kiyo5hi <44930252+Kiyo5hi@users.noreply.github.com>
2023-05-28 22:15:28 +0000

    refactor(house-search): make house search a component

--- source/components/setup.js
+++ source/components/setup.js
@@ -2,8 +2,10 @@ import AppHeader from './AppHeader.js';
 import HomeSection from './HomeSection.js';
 import SortOrChooseSection from './SortOrChooseSection.js';
 import SortingHatSection from './SortingHatSection.js';
+import HouseSearchSection from './HouseSearchSection.js';
 
 customElements.define('app-header', AppHeader);
 customElements.define('home-section', HomeSection);
 customElements.define('sort-or-choose-section', SortOrChooseSection);
 customElements.define('sorting-hat-section', SortingHatSection);
+customElements.define('house-search-section', HouseSearchSection);
