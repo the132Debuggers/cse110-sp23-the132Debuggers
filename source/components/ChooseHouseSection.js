55d578c0dfa38eb59b3055860d3fb056ce3e02b4
Frank(Tianqing) Li <97709181+litianqing2887@users.noreply.github.com>
2023-05-28 17:39:19 -0700

    feat(choose-house): added choose house page (#88)
    * Added ChooseYourHouse part
    
    * Update pictures and js
    
    * Update ChooseHouseSection.js
    
    * Update ChooseHouseSection.js
    
    * chore(*): adjustments
    
    ---------
    
    Co-authored-by: Kiyoshi <44930252+Kiyo5hi@users.noreply.github.com>

---
+++ source/components/ChooseHouseSection.js
@@ -0,0 +1,92 @@
+import { normalize } from '../js/utils.js';
+
+class ChooseHouseSection extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+
+    const style = new CSSStyleSheet();
+    style.replaceSync(`
+        :host {
+          height: 100%;
+          display: flex;
+        }
+
+        #wrapper {
+          display: flex;
+          width: 100%;
+          flex-direction: column;
+          place-items: center;
+          place-content: center;
+        }
+
+        h1 {
+          font-size: 3rem;
+          text-align: center;
+          margin-top: 4rem;
+        }
+
+        #flags {
+          flex: 1;
+          display: grid;
+          grid-template-columns: repeat(4, minmax(0, 1fr));
+          place-items: center;
+          place-content: center;
+          gap: 2rem;
+        }
+
+        .option {
+          order-radius: 0.5rem;
+          padding: 1.5rem;
+          max-width: 12rem;
+        }
+
+        .option:hover {
+          background-color: rgba(0, 0, 0, 0.3);
+          box-shadow: 0 0 0.5rem 0.25rem rgba(0, 0, 0, 0.3);
+        }
+
+        @media (max-width: 768px) {
+          h1 {
+            font-size: 2rem;
+          }
+
+          #flags {
+            grid-template-columns: repeat(2, minmax(0, 1fr));
+          }
+
+          .option {
+            max-width: 8rem;
+            padding: 1rem;
+          }
+        }
+    `);
+
+    this.shadowRoot.adoptedStyleSheets = [normalize, style];
+
+    this.shadowRoot.innerHTML = `
+        <div id="wrapper">
+          <h1>Choose Your House</h1>
+          <div id="flags">
+            <div class="option">
+                <img src="./images/gryffindor/flag.png" alt="Gryffindor" />
+            </div>
+            <div class="option">
+                <img src="./images/ravenclaw/flag.png" alt="Ravenclaw" />
+            </div>
+            <div class="option">
+                <img src="./images/slytherin/flag.png" alt="Slytherin" />
+            </div>
+            <div class="option">
+                <img src="./images/hufflepuff/flag.png" alt="Hufflepuff" />
+            </div>
+          </div>
+        </div>
+    `;
+
+    document.querySelector('#app').style.backgroundImage =
+      'url(./images/choose-house.png)';
+  }
+}
+
+export default ChooseHouseSection;
