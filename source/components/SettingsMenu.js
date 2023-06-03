5564e392a311af945005d686af5bd8b09cbb499f
Pascal H <pascal.huang1@gmail.com>
2023-06-03 15:14:53 -0700

    fix(settings-menu): changing page translation

--- source/components/SettingsMenu.js
+++ source/components/SettingsMenu.js
@@ -1,4 +1,5 @@
 import { normalize } from '../js/utils.js';
+import { translate } from '../js/translation.js';
 
 class SettingsMenu extends HTMLElement{
     constructor(){
@@ -162,53 +163,13 @@ class SettingsMenu extends HTMLElement{
  
 
         const language = this.shadowRoot.querySelector('#language');
-        language.addEventListener('click', () => {
+        language.addEventListener('mousedown', () => {
             localStorage.setItem('previousLanguage',language.value);
         });
         language.addEventListener('change', () => {
+            localStorage.setItem('currentLanguage',language.value);
             translate(language.value);
         });
-
-        var wordsList;
-        fetch('./js/wordsList.json').then(response => response.json())
-        .then(data => {
-                wordsList = data;
-        })
-        .catch(error => {
-        console.error('Error:', error);
-        });
-
-        function replaceWords(inputString, dictionary, sourceLanguage, targetLanguage) {
-            if (dictionary.hasOwnProperty(sourceLanguage) && dictionary[sourceLanguage].hasOwnProperty(inputString)) {
-              const translation = dictionary[sourceLanguage][inputString];
-              if (translation.hasOwnProperty(targetLanguage)) {
-                return translation[targetLanguage];
-              }
-            }
-            return inputString;
-          }
-
-
-        //Does not translate the page when switching,
-        //Can't translate the output from Fortune Teller
-        function translate(currentLanguage){
-            const elements = document.querySelectorAll('*');
-            elements.forEach(element => {
-                if(element.shadowRoot !== null){
-                    element.shadowRoot.querySelectorAll('p, h1, h2, h3, h4, h5, div#notice').forEach(words => {
-                        words.innerHTML = replaceWords(words.innerHTML, wordsList, localStorage.getItem('previousLanguage'), currentLanguage);
-                    })
-                    if(element.shadowRoot.querySelector("input[type=text]") != null){
-                        element.shadowRoot.querySelector("input[type=text]").placeholder = replaceWords(element.shadowRoot.querySelector("input[type=text]").placeholder, wordsList, localStorage.getItem('previousLanguage'), currentLanguage);
-                    }
-                }
-                
-            });
-
-        
-
-        
-        }
     } 
 }
 export default SettingsMenu;
