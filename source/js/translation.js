1a1fea7601d9c1690f33eb2da37a38c2ce76b317
Pascal H <pascal.huang1@gmail.com>
2023-06-04 09:54:29 -0700

    fix(translation.js): hasOwnProperty eslint error

--- source/js/translation.js
+++ source/js/translation.js
@@ -8,30 +8,30 @@ console.error('Error:', error);
 });
 
 function replaceWords(inputString, dictionary, sourceLanguage, targetLanguage) {
-    if (dictionary.hasOwnProperty(sourceLanguage) && dictionary[sourceLanguage].hasOwnProperty(inputString)) {
-      const translation = dictionary[sourceLanguage][inputString];
-      if (translation.hasOwnProperty(targetLanguage)) {
-        return translation[targetLanguage];
-      }
+  if(Object.prototype.hasOwnProperty.call(dictionary,sourceLanguage) && Object.prototype.hasOwnProperty.call(dictionary[sourceLanguage],inputString)){
+    const translation = dictionary[sourceLanguage][inputString];
+    if (Object.prototype.hasOwnProperty.call(translation,targetLanguage)) {
+      return translation[targetLanguage];
     }
-    return inputString;
   }
+  return inputString;
+}
 
 
 //Can't translate the output from Fortune Teller
 export function translate(currentLanguage){
-    localStorage.setItem('currentLanguage',currentLanguage);
-    const elements = document.querySelectorAll('*');
-    elements.forEach(element => {
-        if(element.shadowRoot !== null){
-            element.shadowRoot.querySelectorAll('p, h1, h2, h3, h4, h5, div#notice').forEach(words => {
-                words.innerHTML = replaceWords(words.innerHTML, wordsList, localStorage.getItem('previousLanguage'), currentLanguage);
-            })
-            if(element.shadowRoot.querySelector("input[type=text]") != null){
-                element.shadowRoot.querySelector("input[type=text]").placeholder = replaceWords(element.shadowRoot.querySelector("input[type=text]").placeholder, wordsList, localStorage.getItem('previousLanguage'), currentLanguage);
-            }
-        }
+  localStorage.setItem('currentLanguage',currentLanguage);
+  const elements = document.querySelectorAll('*');
+  elements.forEach(element => {
+      if(element.shadowRoot !== null){
+          element.shadowRoot.querySelectorAll('p, h1, h2, h3, h4, h5, div#notice').forEach(words => {
+              words.innerHTML = replaceWords(words.innerHTML, wordsList, localStorage.getItem('previousLanguage'), currentLanguage);
+          })
+          if(element.shadowRoot.querySelector("input[type=text]") != null){
+              element.shadowRoot.querySelector("input[type=text]").placeholder = replaceWords(element.shadowRoot.querySelector("input[type=text]").placeholder, wordsList, localStorage.getItem('previousLanguage'), currentLanguage);
+          }
+      }
     
         
-    });
+  });
 }
