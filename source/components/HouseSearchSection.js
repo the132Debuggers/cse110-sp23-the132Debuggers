e209318c08fbcb2aaf558200d8716f23f3e8c50e
Leica <leica.shen@gmail.com>
2023-06-09 02:48:42 -0700

    update placeholder text

--- source/components/HouseSearchSection.js
+++ source/components/HouseSearchSection.js
@@ -104,7 +104,7 @@ class HouseSearchSection extends HTMLElement {
         </div>
         <div id="text-area">
             <div id="question">
-                <input type="text" placeholder="Text Here..." autofocus /> 
+                <input type="text" placeholder="Predict Your Fortune..." autofocus /> 
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
                 </svg>
