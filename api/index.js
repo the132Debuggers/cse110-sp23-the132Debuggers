9339124912e5b7e3987ad600225ba3e5d8ef8e9e
shreyagupta112 <94256244+shreyagupta112@users.noreply.github.com>
2023-06-11 02:24:14 -0700

    fixed positioning of restart button

--- api/index.js
+++ api/index.js
@@ -13,22 +13,24 @@ const houseTraits = {
 
 async function query(question, hogwartsHouse) {
   const prompt = `You are a fortune teller in the world of harry potter.
-    Your task is only to answer questions from wizards and witches about their future.
-    Given the question delimited by ''', generate a response to the witches or wizards question.
-    If the question is anything but a question about the wizard/witch's future, respond with "This is not a question about your future, and as a fortune teller I cannot answer this. Try again with a question about your future".
-    If the question is a question about the wizard/witch's future, answer the question by predicting the future. 
-    The wizard/witch is from hogwarts house ${hogwartsHouse} and people from ${hogwartsHouse} house are known to be ${houseTraits[hogwartsHouse]}. 
-    Predict the future of this person based on what you know of their personality and the fact that they go to Hogwarts school in the world of Harry Potter.
-    Try to include specific elements of the magical harry potter world in your reponse.
-    Your response has to be 250 characters long or less.
-    Question: '''${question}''' 
-    `;
+  Given the question delimited by ''', generate a response which is less than 25 words.
+  If the question is anything but a question about the wizard/witch's future, respond with "This is not a question about your future. Try again with a question about your future".
+  If the question is a question about the wizard/witch's future, answer the question by making up a prediction for the future.
+  The wizard/witch is from hogwarts house ${hogwartsHouse} and people from ${hogwartsHouse} house are known to be ${houseTraits[hogwartsHouse]}.
+  Answer the person's question based on their personality and the fact that they go to Hogwarts school in the world of Harry Potter.
+  Try to include specific elements of the magical harry potter world in your reponse.
+  Your response must be less than 25 words long (this is the most important rule).
+  Question: '''${question}'''
+  `;
 
   const response = await openai.createCompletion({
     model: 'text-davinci-003',
     prompt,
-    max_tokens: 100,
     temperature: 0.6,
+    max_tokens: 256,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0
   });
 
   const result = response.data.choices[0].text;
