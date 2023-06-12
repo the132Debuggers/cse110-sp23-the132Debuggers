bd593fbfbae0aae5f8628495ffa78b916a700c0d
Shreya Gupta <94256244+shreyagupta112@users.noreply.github.com>
2023-06-11 22:12:44 -0700

    Revert "Revert "feat(speech): changed speech align with page" (#160)"
    This reverts commit 1cb178aaceee9a5c2ee53e5c8b8a126b32e52c77.

--- source/js/audio.js
+++ source/js/audio.js
@@ -1,9 +1,22 @@
 function isMuted() {
   return localStorage.getItem('isMuted') === 'true';
 }
+function stopAudio() {
+  return localStorage.getItem('stopAudio') === 'true';
+}
 
 function toggleMute() {
   localStorage.setItem('isMuted', !isMuted());
 }
 
-export { isMuted, toggleMute };
+function switchStop(i) {
+  if (i === 'false') {
+    localStorage.setItem('stopAudio', false);
+  } else {
+    localStorage.setItem('stopAudio', true);
+  }
+}
+
+export {
+  isMuted, toggleMute, stopAudio, switchStop
+};
