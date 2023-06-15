9f166b913156a7556f6bbfd93d245d9dfe4ca2c1
Grace <45552424+yyygrace@users.noreply.github.com>
2023-06-14 23:56:45 -0700

    test: added tests for query (#173)
    * add query tests
    
    * prettier
    
    * Update fortunes.js
    
    * prettier
    
    * Update query.test.js

---
+++ source/tests/query.test.js
@@ -0,0 +1,19 @@
+import query from '../js/fortunes.js';
+
+describe('query', () => {
+  test('should not be a question', async () => {
+    const input = 'not a question';
+    const output = await query(input, 'Hufflepuff');
+    const expected = 'This is not a question about your future';
+    expect(output).toContain(expected);
+  }, 11000);
+});
+
+describe('query', () => {
+  test('should be a question', async () => {
+    const input = 'will I get an A in CSE 110?';
+    const output = await query(input, 'Slytherin');
+    const notExpected = 'This is not a question about your future';
+    expect(output).not.toContain(notExpected);
+  }, 11000);
+});
